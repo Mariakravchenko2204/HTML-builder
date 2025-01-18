22918efafbfa6bef3c47b235fd6d111552408104
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2025-01-17 19:58:53 -0500

    fix: wrap script into function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
-const folderPath = path.join(__dirname, 'files');
-const copyFolderPath = path.join(__dirname, 'files-copy');
+const copyDir = () => {
+  const folderPath = path.join(__dirname, 'files');
+  const copyFolderPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(copyFolderPath, (err) => {
-  if (err) throw err;
-});
-
-fs.promises.readdir(folderPath).then((files) => {
-  files.map((file) => {
-    fs.copyFile(
-      path.join(folderPath, file),
-      path.join(copyFolderPath, file),
-      (err) => {
-        if (err) throw err;
-      },
-    );
+  fs.mkdir(copyFolderPath, (err) => {
+    if (err) throw err;
   });
-});
+
+  fs.promises.readdir(folderPath).then((files) => {
+    files.map((file) => {
+      fs.copyFile(
+        path.join(folderPath, file),
+        path.join(copyFolderPath, file),
+        (err) => {
+          if (err) throw err;
+        },
+      );
+    });
+  });
+};
+
+copyDir();
